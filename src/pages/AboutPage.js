ec3737762224db28e7710ba4abdb0cf439fcc35b
microgssd <microgssd@gmail.com>
2026-09-01 01:48:16 +0530

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -253,7 +253,7 @@ export default function AboutPage({ go }) {
               textTransform:'uppercase'
             }}
           >
-            CEO & Development Head
+            Development Head
           </p>
         </div>
       </div>
